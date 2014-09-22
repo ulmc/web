4005c1d7dc84cb9e0c621b17d19485034162fc63
Alex <a.kolmogorov@sigma-it.ru>
2014-09-22 11:52:37 +0400

    InitialCommit

---
+++ _include/page/advantages.php
@@ -0,0 +1,112 @@
+<div id="about" class="page-alternate">
+    <div class="container">
+        <!-- Title Page -->
+        <div class="row">
+            <div class="span12">
+                <div class="title-page">
+                    <h2 class="title">Особенности</h2>
+
+                    <h3 class="title-description">Ключевые особенности концепции.</h3>
+                </div>
+            </div>
+        </div>
+        <!-- End Title Page -->
+
+        <!-- Фишки -->
+        <div class="row">
+
+            <!-- Start Thing -->
+            <div class="span4 profile">
+                <div class="image-wrap">
+                    <div class="hover-wrap">
+                        <span class="overlay-img"></span>
+                        <span class="overlay-text-thumb">Продуманная история</span>
+                    </div>
+                    <img src="_include/img/things/thing-01.jpg" alt="История Ультимуса">
+                </div>
+                <h3 class="profile-name">Лор</h3>
+
+                <p class="profile-description">
+                    Войска эндера вторгаются, в результате кровополитной войны в обществе произошел раскол, некоторые
+                    остались
+                    приверженцами старой школы(только олдскул, только хардкор!) и ударились в магию, другие положились
+                    на практические
+                    знания бывших учеников волшебников, которых теперь зовут учёными. Вот, можно сделать таким образом
+                    две рукотворные
+                    локации, затопленная часть ультимуса - старые, разрушенные порты. Ушедший под землю СЭЗ, в
+                    результате истощительной
+                    добычи ресорсов технократами. Все дела.
+                </p>
+
+                <!--div class="social">
+                    <ul class="social-icons">
+                        <li><a href="#"><i class="font-icon-social-twitter"></i></a></li>
+                        <li><a href="#"><i class="font-icon-social-dribbble"></i></a></li>
+                        <li><a href="#"><i class="font-icon-social-facebook"></i></a></li>
+                    </ul>
+                </div-->
+            </div>
+            <!-- End Thing -->
+
+            <!-- Start Thing -->
+            <div class="span4 profile">
+                <div class="image-wrap">
+                    <div class="hover-wrap">
+                        <span class="overlay-img"></span>
+                        <span class="overlay-text-thumb">Динамический мир</span>
+                    </div>
+                    <img src="_include/img/things/thing-02.jpg" alt="Динамический мир">
+                </div>
+                <h3 class="profile-name">Динамический мир</h3>
+
+                <p class="profile-description">
+                    Ну тут я хз. Зима там всякая хуё-моё. Сами придумывайте что сюда писать.
+                </p>
+
+                <nav class="icons-alt">
+                    <ul>
+                        <li><a href="href=https://github.com/ulmc/ModPack/"><span class="font-icon-social-github"></span></a></li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- End Thing -->
+
+            <!-- Start Thing -->
+            <div class="span4 profile">
+                <div class="image-wrap">
+                    <div class="hover-wrap">
+                        <span class="overlay-img"></span>
+                        <span class="overlay-text-thumb">Модификации</span>
+                    </div>
+                    <img src="_include/img/things/thing-03.jpg" alt="Модификации">
+                </div>
+                <h3 class="profile-name">Модификации</h3>
+
+                <p class="profile-description">
+                    Ну описать конечно надо, но вот что есть.
+                    Посмертный контейнер - блок костей.<br>
+                    Grinder - точильные камни, спец. блок.<br>
+                    ThievesCraft - Приватный сундук, запираемый на ключ, отмычки, ключи, ловушки и т.п.<br>
+                    Survival Season: Штрафы и повреждения от нахождения в экстримальных биомах от холода или жары.
+                    Можно греться рядом с огнём, замерзать насмерть в шторм, купаясь в ледяной воде, или укрываться от
+                    палящего солнца
+                    пустыни. Дополнительные элементы одежды - теплая одежда, утепленная боевая броня, легкая одежда.
+                    Напитки/микстуры/алкоголь/настойки. Новые эффекты.<br>
+                    Специальная броня и одежда<br>
+                    CraftRecipies - рецепты крафта<br>
+                </p>
+
+                <nav class="icons-alt">
+                    <ul>
+                        <li><a href="https://github.com/ulmc/ModPack/"><span class="font-icon-social-github"></span></a></li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+        <!-- End Thing -->
+
+    </div>
+    <!-- End Things -->
+</div>
+</div>
